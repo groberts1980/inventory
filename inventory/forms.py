67099ed464584e960d8f0bd3dac5990b2eefbdd7
Greg Roberts <groberts1980@gmail.com>
2020-03-07 13:22:28 -0700

    More tinkering

--- inventory/forms.py
+++ inventory/forms.py
@@ -1,5 +1,8 @@
 import models
 
+class Category(models.Model):
+    category = CharField(max_length=50)
+
 class Item(forms.Modelform):
     name     = CharField(required=True, max_length=50)
     category = CharField(required=True, max_length=50)
